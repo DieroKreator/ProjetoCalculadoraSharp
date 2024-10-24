febe52610f71abc22ad7e358abf46c3c66aaf009
DieroKreator <qtechwired@gmail.com>
2024-10-24 09:07:21 -0300

    Added 3 more operation tests

--- Calculadora139Sharp.Tests/CalculadoraTests.cs
+++ Calculadora139Sharp.Tests/CalculadoraTests.cs
@@ -5,11 +5,10 @@ namespace Calculadora139Sharp.Tests;
 
 public class Tests
 {
-    // private Calculadora _calculadora;
     [SetUp]
     public void Setup()
     {
-        // _calculadora = new Calculadora(); // instancia o objeto
+
     }
 
     [Test]
@@ -20,9 +19,48 @@ public class Tests
         int num1 = 15;
         int num2 = 35;
 
-        int resultadoEsperado =50;
+        int resultadoEsperado = 50;
 
         int resultadoAtual = Calculadora.SomarDoisNumero(num1, num2);
+
+        Assert.That(resultadoEsperado, Is.EqualTo(resultadoAtual));
+    }
+
+    [Test]
+    public void testSubtrairDoisNumeros()
+    {
+        int num1 = 20;
+        int num2 = 8;
+
+        int resultadoEsperado = 12;
+
+        int resultadoAtual = Calculadora.SubtrairDoisNumeros(num1, num2);
+
+        Assert.That(resultadoEsperado, Is.EqualTo(resultadoAtual));
+    }
+
+    [Test]
+    public void testMultiplicarDoisNumeros()
+    {
+        int num1 = 20;
+        int num2 = 1;
+
+        int resultadoEsperado = 20;
+
+        int resultadoAtual = Calculadora.MultiplicarDoisNumeros(num1, num2);
+
+        Assert.That(resultadoEsperado, Is.EqualTo(resultadoAtual));
+    }
+
+    [Test]
+    public void testDividirDoisNumeros()
+    {
+        int num1 = 20;
+        int num2 = 4;
+
+        int resultadoEsperado = 5;
+
+        int resultadoAtual = Calculadora.DividirDoisNumeros(num1, num2);
 
         Assert.That(resultadoEsperado, Is.EqualTo(resultadoAtual));
     }
